364f3406b6f6490b9a7399c7a45a2bc177a4cea0
ankitubale <ankitubale3@gmail.com>
2021-11-03 19:12:42 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -11,23 +11,6 @@ module "vpc" {
             subnet_name           = "subnet-01"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = var.region
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = var.region
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
         }
     ]
 
@@ -38,8 +21,6 @@ module "vpc" {
                 ip_cidr_range = "192.168.64.0/24"
             },
         ]
-
-        subnet-02 = []
     }
 
     routes = [
